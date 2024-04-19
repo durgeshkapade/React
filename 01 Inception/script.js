bc69d5ceb8ae5e44ab92154ceb69d4571df3165e
durgeshkapade <kapadedurgesh6@gmail.com>
2024-04-19 23:25:45 +0530

    React first project

--- 01 Inception/script.js
+++ 01 Inception/script.js
@@ -10,6 +10,9 @@
 // );
 
 
+
+
+
 let heading =  React.createElement("div",{id:"parent"},[
         React.createElement("div",{id:"child1"},
             [React.createElement("h1",{},"Hello world welcome to react"),
